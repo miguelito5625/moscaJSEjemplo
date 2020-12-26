7c946cde3b74be6c72a98544063c3abb12dd3e17
exbinario <miguel.4to.1994@gmail.com>
2020-12-25 22:03:07 -0600

    first commit

---
+++ src/subscriber.js
@@ -0,0 +1,11 @@
+var mqtt = require('mqtt');
+var client = mqtt.connect('mqtt://localhost');
+
+client.on('connect', function () {
+    client.subscribe('myTopic')
+});
+
+client.on('message', function (topic, message) {
+    context = message.toString();
+    console.log(context)
+});
